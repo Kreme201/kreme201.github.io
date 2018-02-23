2cf20ad750a8f14fc65b04ec67c7bb5eb9d29e05
JT-201 <201@studio-jt.co.kr>
2018-02-23 10:11:46 +0900

    add font awesome

--- sanha/scripts.js
+++ sanha/scripts.js
@@ -1,20 +1,9 @@
 jQuery( function ( $ ) {
 
-/*
-        <audio id="audio" autoplay="autoplay" loop="true" controls="true" preload="none">
-            <source src="./assets/audio/music.mp3" />
-        </audio>
-*/
-    var $audio = $( '<audio />', {
-        'autoplay' : 'autoplay',
-        'loop'     : 'true',
-        'controls' : 'false',
-        'preload'  : 'none',
-        'style'    : 'display:none;',
-        'html'     : $( '<source />', { 'src': './assets/audio/music.mp3' } )
-    } );
-    $audio.appendTo( $( 'body' ) );
-    $( 'span.audio_box' ).css( 'opacity', $audio[ 0 ].paused ? '.5' : '.3' );
+    var $audio = $( 'audio' );
+
+    $audio.on( 'playing', function () { $( 'span.audio_box' ).css( 'opacity', '.5' ); } );
+    $audio.on( 'pause', function () { $( 'span.audio_box' ).css( 'opacity', '.3' ); } );
 
     $( 'span.audio_box' ).on( 'click', function () {
 
@@ -37,17 +26,18 @@ jQuery( function ( $ ) {
 
     var arr_symbol = [ '&#9728;', '&#8277;', '&#9733;', '&#9794;' ];
     var symbol     = arr_symbol[ parseInt( Math.random() * 10 ) % arr_symbol.length ];
+    symbol = '&#9728;';
 
     setInterval( function () {
 
-        var options           = { minSize: 10, maxSize: 20, color: '#ddd' };
+        var options           = { minSize: 10, maxSize: 15, color: '#e0dbd5' };
         var startPositionLeft = Math.random() * $( document ).width();
         var startOpacity      = 0.5 + Math.random();
         var sizeFlake         = options.minSize + Math.random() * options.maxSize;
         var endPositionTop    = $( document ).height() - 40;
         var endPositionLeft   = startPositionLeft - 100 + Math.random() * 200;
         var durationFall      = $( document ).height() * 10 + Math.random() * 10000;
-        var $flake            = $('<span />', { class: 'flake', style: 'display:block;position:absolute;top:0px;', html: symbol } );
+        var $flake            = $('<i />', { class: 'flake fas fa-snowflake', style: 'display:block;position:absolute;top:0px;' } );
 
         $flake.appendTo('body');
         $flake.css( { left: startPositionLeft, opacity: startOpacity, 'font-size': sizeFlake, color: options.color } )
