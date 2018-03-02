720610ed581bae54d866487a00e5dc6008df5ea7
JT-201 <201@studio-jt.co.kr>
2018-03-02 18:16:24 +0900

    이미지 갤러리 추가

--- sanha/scripts.js
+++ sanha/scripts.js
@@ -5,7 +5,9 @@ jQuery( function ( $ ) {
     $audio.on( 'playing', function () { $( 'span.audio_box' ).css( 'opacity', '.5' ); } );
     $audio.on( 'pause', function () { $( 'span.audio_box' ).css( 'opacity', '.3' ); } );
 
-    $( 'span.audio_box' ).on( 'click', function () {
+    $( 'span.audio_box' ).on( 'click', function ( e ) {
+
+        e.preventDefault();
 
         var $target = $( 'audio' )[ 0 ];
 
@@ -23,7 +25,27 @@ jQuery( function ( $ ) {
 
     } );
 
+    $( '.photo_box' ).click( function( e ) {
+
+        e.preventDefault();
+
+        var data = [];
+
+        for ( var i = 1; i <= 16; i++ ) {
+
+            data.push( { href: './assets/gallery/images/' + i + '.jpg', title: '이산하' } );
+
+        }
+
+        $.swipebox( data );
+
+        return false;
+
+    } );
+
     setInterval( function () {
+
+        if ( $( 'i.flake' ).length < 10 ) {
 
         var options           = { minSize: 5, maxSize: 10, color: '#e0dbd5' };
         var startPositionLeft = Math.random() * $( document ).width();
@@ -31,12 +53,14 @@ jQuery( function ( $ ) {
         var sizeFlake         = options.minSize + Math.random() * options.maxSize;
         var endPositionTop    = $( document ).height() - 40;
         var endPositionLeft   = startPositionLeft - 100 + Math.random() * 200;
-        var durationFall      = $( document ).height() * 10 + Math.random() * 10000;
+        var durationFall      = 10000;
         var $flake            = $('<i />', { class: 'flake fas fa-asterisk', style: 'display:block;position:absolute;top:0px;' } );
 
         $flake.appendTo('body');
         $flake.css( { left: startPositionLeft, opacity: startOpacity, 'font-size': sizeFlake, color: options.color } )
-        $flake.animate( { top: endPositionTop, left: endPositionLeft, opacity: 0.5 }, durationFall, 'linear', function () { $( this ).remove(); } );
+        $flake.animate( { top: endPositionTop, opacity: 0.5 }, durationFall, 'linear', function () { $( this ).remove(); } );
+
+        }
 
     }, 1000 );
 
